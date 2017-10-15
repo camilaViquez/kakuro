c6b7639b1d05c4452ab7750ad029fae70c0e07bc
camilaViquez <32073514+camilaViquez@users.noreply.github.com>
2017-10-15 16:52:07 +0100

    mr

--- kakuros.py
+++ kakuros.py
@@ -101,23 +101,21 @@ def accesarTablero(tablero):
 
 def poda(posicion, debeSumarF, debeSumarC):
     valores = set('123456789')
-    print("debeSumarf: " +str(debeSumarF))
-    print("debeSumarC: " +str(debeSumarC))
     
     if(str(debeSumarF)!= 'none'):
-        print("validacion: " + str(debeSumarF))
         lis_posiblesF = crearListaPosibles(debeSumarF)
-        print("lista: " + str(lis_posiblesF))
-        conj_posiblesF = set(l_posiblesF)
-        print("conjuntos: " + str(conj_posiblesF))
-        
+        conj_posiblesF = set(l_posiblesF)        
     if(str(debeSumarC)!= 'none'):
-        print("validacionbu: " + str(debeSumarC))
         lis_posiblesC = crearListaPosibles(debeSumarC)
-        conj_posiblesC = set(l_posiblesC)
-    else:
-        print("hol2")
+        conj_posiblesF = set(l_posiblesC)
+    #interseccion de ambos conjuntos para eliminar valores que no sirven
+        interseccion = conj_posiblesF & conj_posiblesC
+        
+        
+        
     
+        
+  
     #llamar backTrking
 
     
@@ -141,7 +139,7 @@ def backTraking(prueba, pp):
     copia = pp
     suma = 9
     while copia != []:
-        if(prueba[0] + pp[0] == suma):
+        if(prueba[0] + copia[0] == suma):
             resulta.append(prueba[0])
             resulta.append(pp[0])
             #resulta = resulta[copia[0], pp[0]]
@@ -153,8 +151,47 @@ def backTraking(prueba, pp):
     del prueba[0]
     return backTraking(prueba, pp)
 
+ #partiendo del hecho de que tengo la union de mis tres casiilas(le paso pisibles y can casillas y la suma de la fila)
+def conjuntoPotencia():
+    posibles = [1,2,3,4,5,6,7,8,9]#union
+    cantCasillas = 3
+    sumaF = 13
+    comb = list(combinations(posibles, cantCasillas))
+    evaluarConjunto(comb, sumaF)
+
+
+def evaluarConjunto(comb, sumaF):
+    cont = 0
+    lista = []   
+    for i in comb:
+        if isinstance(i,tuple):
+            evaluarConjunto(i,sumaF)
+        else:
+                lista.append(i)
+    validarSuma(lista, sumaF)
+
+#validar los conjuntos que suman la suma que indica la llave, y asi permitar lo minimo posible
+def validarSuma(lista, sumaF):
+    resulta = []
+    if lista != []:
+        if(lista[0] + lista[1] + lista[2] == sumaF):
+            resulta.append(lista)
+            print("resulta: ", resulta)
+            permutarConjunto(resulta)
+            return resulta
+    #print("resulta: ", resulta)
             
-   
+listaPosibles = []
+def permutarConjunto(lista):
+    global listaPosibles
+    if(lista !=[]):
+        per = list(permutations(lista[0]))
+        listaPosibles.append(per)
+    imprimir(listaPosibles)
+    
+def imprimir(listaPosibles):
+    for i in listaPosibles:
+        print(i)   
 
 
     
@@ -167,7 +204,8 @@ def backTraking(prueba, pp):
     
         
         
-'''     
+'''
+Kakuros implementado con clases
 
 #celda = {'celdaNegra' : {'color' : 'negro', 'sumaF' : 'none', 'sumaC':'none'},'celdaBlanca':{'color' : 'blanco', 'valor' : '0' } }
 
@@ -397,6 +435,147 @@ def draw():
            
 '''
 
+'''
+#partiendo del hecho de que tengo la union de mis tres casiilas(le paso pisibles y can casillas y la suma de la fila)
+def conjuntoPotencia(sumaF_1, sumaF_2):
+    posibles = [1,2,3,4,5,6,7,8,9]#union
+    cantCasillas = 3
+    comb = list(combinations(posibles, cantCasillas))
+    evaluarConjunto(comb, sumaF_1, sumaF_2)
 
 
+def evaluarConjunto(comb, sumaF_1, sumaF_2):
+    cont = 0
+    lista_1 = []
+    lista_2 = [] 
+    for i in comb:
+        if isinstance(i,tuple):
+            evaluarConjunto(i,sumaF_1, sumaF_2)
+        else:
+                lista_1.append(i)
+                lista_2.append(i)
+    #print("lista_1: ",lista_1)
+    #print("lista_2: ",lista_2)
+                
+    validarSuma(lista_1, lista_2, sumaF_1, sumaF_2)
+
+#validar los conjuntos que suman la suma que indica la llave, y asi permitar lo minimo posible
+resulta_1 = []
+resulta_2 = []
+def validarSuma(lista_1, lista_2, sumaF_1, sumaF_2):
+    global resulta_1, resulta_2
+    if lista_1 != []:
+        if(lista_1[0] + lista_1[1] + lista_1[2] == sumaF_1):
+            resulta_1.append(lista_1)
+    if lista_2 != []:
+        if(lista_2[0] + lista_2[1] + lista_2[2] == sumaF_2):
+            resulta_2.append(lista_2)
+    permutarConjunto(resulta_1, resulta_2)
     
+listaPosibles_1 = []
+listaPosibles_2 = []   
+def permutarConjunto(lista_1, lista_2):
+    global listaPosibles_1, listaPosibles_2
+    if(lista_1 !=[]):
+        per_1 = list(permutations(lista_1[0]))
+        listaPosibles_1.append(per_1)
+    if(lista_2 !=[]):
+        per_2 = list(permutations(lista_2[0]))
+        listaPosibles_2.append(per_2)
+        
+    productoCartesiano(listaPosibles_1, listaPosibles_2)
+    
+
+def productoCartesiano(listaPosibles_1, listaPosibles_2):
+    print(listaPosibles_1)
+    print(listaPosibles_2)
+    #http://elclubdelautodidacta.es/wp/2013/05/python-el-producto-cartesiano/
+    return([(k,n)for i in listaPosibles_1 for j in listaPosibles_2 for k in i for n in j])
+'''
+'''
+from math import factorial
+from itertools import combinations, permutations
+
+def potencia(c):
+    """Calcula y devuelve el conjunto potencia del 
+       conjunto c.
+    """
+    if len(c) == 0:
+        return [[]]
+    r = potencia(c[:-1])
+    return r + [s + [c[-1]] for s in r]
+
+def imprime_ordenado(c):
+    """Imprime en la salida estándar todos los
+       subconjuntos del conjunto c (una lista de
+       listas) ordenados primero por tamaño y
+       luego lexicográficamente. Cada subconjunto
+       se imprime en su propia línea. Los
+       elementos de los subconjuntos deben ser
+       comparables entre sí, de otra forma puede
+       ocurrir un TypeError.
+    """
+    for e in sorted(c, key=lambda s: (len(s), s)):
+        print(e)
+
+def combinaciones(c, n):
+    """Calcula y devuelve una lista con todas las
+       combinaciones posibles que se pueden hacer
+       con los elementos contenidos en c tomando n
+       elementos a la vez.
+    """
+    return [s for s in potencia(c) if len(s) == n]
+
+def inserta(x, lst, i):
+    """Devuelve una nueva lista resultado de insertar
+       x dentro de lst en la posición i.
+    """
+    return lst[:i] + [x] + lst[i:]
+
+
+def inserta_multiple(x, lst):
+    """Devuelve una lista con el resultado de
+       insertar x en todas las posiciones de lst.  
+    """
+    return [inserta(x, lst, i) for i in range(len(lst) + 1)]
+
+def permuta(c):
+    """Calcula y devuelve una lista con todas las
+       permutaciones posibles que se pueden hacer
+       con los elementos contenidos en c.
+    """
+    if len(c) == 0:
+        return [[]]
+    return sum([inserta_multiple(c[0], s)
+                for s in permuta(c[1:])],
+               [])
+
+def permutaciones(c, n):
+    """Calcula y devuelve una lista con todas las
+       permutaciones posibles que se pueden hacer
+       con los elementos contenidos en c tomando n
+       elementos a la vez.
+    """
+    return sum([permuta(s)
+                for s in combinaciones(c, n)],
+               [])
+listaR_1 = []
+listaR_2 = []
+def prueba(sumaF_1, sumaF_2):
+    lista = permutaciones([1,2,3,4,5,6,7,8,9],3)
+    for i in lista:
+        validarSuma(i, sumaF_1, sumaF_2)
+        
+def validarSuma(valor, sumaF_1, sumaF_2):
+    print("valor", valor)
+    global listaR_1,listaR_2
+    if(valor [0]+ valor[1] + valor[2] == sumaF_1):
+        listaR_1.append(valor)
+    if(valor [0]+ valor[1] + valor[2] == sumaF_2):
+        listaR_2.append(valor)
+
+def camila(listaR_1, listaR_2):
+    print("lista del 13: ",listaR_1,"\n")
+    print("lista del 6: ",listaR_2,"\n")
+
+'''    
