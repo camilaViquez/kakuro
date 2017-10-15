56008012a0a3d302b9a743aecf890f180a188e39
camilaViquez <32073514+camilaViquez@users.noreply.github.com>
2017-10-15 01:03:14 +0100

    permutations and combinations

--- kakuros.py
+++ kakuros.py
@@ -1,6 +1,173 @@
 import random
 
-      
+
+
+def crearTablero (fila,columna):
+    print("hola")
+    tablero = []
+    subLis = [0,0,0,0]
+    tamano = fila * columna
+    for i in range(tamano):
+        tablero.append(subLis)
+    print(tablero)
+    llenarTablero(tablero)
+
+
+def llenarTablero(tablero):
+    c_negra = ["negra",0,0,0]
+    c_blanca = ["blanca",0,0,0]
+
+    tablero [0] = c_negra
+    tablero [1] = ["negra","none",3, "none"]
+    tablero [2] = ["negra","none",20, "none"] 
+    tablero [3] = ["negra","none","none", "none"]   
+    tablero [4] = ["negra","none",10, "none"]
+    tablero [5] = ["negra","none",12, "none"]
+    tablero [6] = ["negra","none",3, "none"]
+    tablero [7] = c_negra
+    tablero [8] = ["negra", 9, "none", "none"]
+    tablero [9] = c_blanca
+    tablero [10] = c_blanca
+    tablero [11] = ["negra",13,"none", "none"]
+    tablero [12] = c_blanca
+    tablero [13] = c_blanca
+    tablero [14] = c_blanca
+    tablero [15] = c_negra
+    tablero [16] = ["negra",11,"none", "none"]
+    tablero [17] = c_blanca
+    tablero [18] = c_blanca
+    tablero [19] = ["negra",6,15, "none"]
+    tablero [20] = c_blanca
+    tablero [21] = c_blanca
+    tablero [22] = c_blanca
+    tablero [23] = c_negra
+    tablero [24] = c_negra
+    tablero [25] = ["negra",6,"none", "none"]
+    tablero [26] = c_blanca
+    tablero [27] = c_blanca
+    tablero [28] = c_blanca
+    tablero [29] = ["negra","none",13, "none"]
+    tablero [30] = c_negra
+    tablero [31] = c_negra
+    tablero [32] = c_negra
+    tablero [33] = ["negra","none",12, "none"]
+    tablero [34] = ["negra",31,4, "none"]
+    tablero [35] = c_blanca
+    tablero [36] = c_blanca
+    tablero [37] = c_blanca
+    tablero [38] = ["negra","none",3, "none"]
+    tablero [39] = c_negra
+    tablero [40] = ["negra",6,"none", "none"]
+    tablero [41] = c_blanca
+    tablero [42] = c_blanca
+    tablero [43] = c_blanca
+    tablero [44] = ["negra",4,"none", "none"]
+    tablero [45] = c_blanca
+    tablero [46] = c_blanca
+    tablero [47] = c_negra
+    tablero [48] = ["negra",16,"none", "none"]
+    tablero [49] = c_blanca
+    tablero [50] = c_blanca
+    tablero [51] = c_blanca
+    tablero [52] = ["negra",3,"none", "none"]
+    tablero [53] = c_blanca
+    tablero [54] = c_blanca
+    tablero [55] = c_negra
+    tablero [56] = c_negra
+    tablero [57] = c_negra
+    tablero [58] = c_negra
+    tablero [59] = c_negra
+    tablero [60] = c_negra
+    tablero [61] = c_negra
+    tablero [62] = c_negra
+    tablero [63] = c_negra
+    #print(tablero)
+    
+    accesarTablero(tablero)
+
+
+#func recursiva para acceder a los datos de subLista
+def accesarTablero(tablero):
+    for posicion in tablero:
+        if isinstance(posicion, list):
+            accesarTablero(posicion)
+        else:
+            print("\n\t","|",posicion,"|")
+
+        
+        
+    
+
+
+def poda(posicion, debeSumarF, debeSumarC):
+    valores = set('123456789')
+    print("debeSumarf: " +str(debeSumarF))
+    print("debeSumarC: " +str(debeSumarC))
+    
+    if(str(debeSumarF)!= 'none'):
+        print("validacion: " + str(debeSumarF))
+        lis_posiblesF = crearListaPosibles(debeSumarF)
+        print("lista: " + str(lis_posiblesF))
+        conj_posiblesF = set(l_posiblesF)
+        print("conjuntos: " + str(conj_posiblesF))
+        
+    if(str(debeSumarC)!= 'none'):
+        print("validacionbu: " + str(debeSumarC))
+        lis_posiblesC = crearListaPosibles(debeSumarC)
+        conj_posiblesC = set(l_posiblesC)
+    else:
+        print("hol2")
+    
+    #llamar backTrking
+
+    
+def crearListaPosibles(rango):
+    print("rango" + str(rango))
+    if(rango != 'none'):
+        lista = []
+        num = 1
+        while(num <= rango):
+            lista = lista.append(num)
+            num += 1
+            print (lista)
+        return lista
+    else:
+        print("hola")
+
+
+
+def backTraking(prueba, pp):
+    resulta = []
+    copia = pp
+    suma = 9
+    while copia != []:
+        if(prueba[0] + pp[0] == suma):
+            resulta.append(prueba[0])
+            resulta.append(pp[0])
+            #resulta = resulta[copia[0], pp[0]]
+            print("resulta suma: " + str(resulta))
+            return resulta
+        else:
+            del copia[0]
+            print("lista recortada: " + str(copia))
+    del prueba[0]
+    return backTraking(prueba, pp)
+
+            
+   
+
+
+    
+        
+
+                
+                
+            
+    
+    
+        
+        
+'''     
 
 #celda = {'celdaNegra' : {'color' : 'negro', 'sumaF' : 'none', 'sumaC':'none'},'celdaBlanca':{'color' : 'blanco', 'valor' : '0' } }
 
@@ -47,21 +214,6 @@ class Celda:
         self.__valor = valor
         
  
-def llaveTablero():
-    global celda,tablero
-    c_negra = celda.get('celdaNegra')
-    print("c negra: " + str(c_negra))
-    c_blanca = celda.get('celdaBlanca')
-    print("c blanca: " + str(c_blanca))
-    #sum_col = c_negra.get('sumaC')
-    #sum_fila = c_blanca.get('sumaF')
-    print(tablero)
-    kakuro(tablero,c_negra, c_blanca)
-    #recorrerTablero()
-
-
-
-
 def rand():
     cont =0
     while cont<100:
@@ -137,30 +289,25 @@ def kakuro(tablero):
     tablero [61] = c_negra
     tablero [62] = c_negra
     tablero [63] = c_negra
+    print (tablero)
+    
+    
+    
+    print ("Color tablero en posicion 1: " + str(tablero[1].color))
+    
 
-    print(tablero)
-
-   
-
-def actualizarLlave(dic, valorF, valorC):
-    #print(dic)
-    copia = {'color' : 'negro', 'sumaF' : valorF, 'sumaC': valorC}
-    dic.update(copia)
-    return dic
-
-def recorrerTablero():
+      
+def recorrerTablero(tablero):
     print("recorrer tablero entra")
     casillaJuego = 9
     posi_actual = tablero[casillaJuego]
     print("posiiocn actuar: " + str(posi_actual))
     posi_llaveF = tablero[casillaJuego - 1]
     print("posi:llaveF: " + str(posi_llaveF))
-    print("casilla 8: " + str(tablero[8]))
-    print("casilla 16: " + str(tablero[16]))
     posi_llaveC = tablero[casillaJuego - 6]
     print("posi:llaveC: " + str(posi_llaveC))
-    debeSumarF = posi_llaveF.get('sumaF')
-    debeSumarC = posi_llaveC.get('sumaC')
+    debeSumarF = posi_llaveF.sumaF
+    debeSumarC = posi_llaveC.sumaC
     #llega a una casilla blanca entonces llama a poda
     #mandarle celdas negras con claves
     poda(posi_actual, debeSumarF, debeSumarC)
@@ -179,9 +326,10 @@ def poda(posicion, debeSumarF, debeSumarC):
         print("lista: " + str(lis_posiblesF))
         conj_posiblesF = set(l_posiblesF)
         print("conjuntos: " + str(conj_posiblesF))
+        
     if(str(debeSumarC)!= 'none'):
         print("validacionbu: " + str(debeSumarC))
-        lis_posiblesC = crearListaPosibles(debeSemarC)
+        lis_posiblesC = crearListaPosibles(debeSumarC)
         conj_posiblesC = set(l_posiblesC)
     else:
         print("hol2")
@@ -219,7 +367,7 @@ def backTraking(prueba, pp):
             del copia[0]
             print("lista recortada: " + str(copia))
     del prueba[0]
-    return func(prueba, pp)
+    return backTraking(prueba, pp)
 
 
     
@@ -247,6 +395,7 @@ def draw():
         y = y + w
         x = 0
            
+'''
 
 
 
